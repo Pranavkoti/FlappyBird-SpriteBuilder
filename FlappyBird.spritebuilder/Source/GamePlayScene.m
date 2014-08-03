3bdd02125348f08e77ca054c52c7f19a7eed5ee8
pranavpravin <pranavpravin@pranavpravins-iMac.local>
2014-08-03 16:41:52 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -15,7 +15,7 @@ character = (Character*)[CCBReader load:@"Character"];
 
 timeSinceObstacle += delta;
     
-  if (timeSinceObstacle > 0.001f)
+  if (timeSinceObstacle > 0.5f)
     {
   
          // Add a new obstacle
