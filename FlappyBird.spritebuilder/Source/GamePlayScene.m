84a0bed2ed8763e9c3ae6fcc82563c6f4abec486
pranavpravin <pranavpravin@pranavpravins-iMac.local>
2014-08-03 16:39:20 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,7 +9,7 @@
     // your code here
     [self addObstacle];
     timeSinceObstacle = 1.0f;
-    count = 5;
+    count = 1;
 character = (Character*)[CCBReader load:@"Character"];
 [physicsNode addChild:character];
 
