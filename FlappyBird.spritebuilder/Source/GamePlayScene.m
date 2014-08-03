a67ddbda4086fd46216473dba775bb6a0ac8587b
pranavpravin <pranavpravin@pranavpravins-iMac.local>
2014-08-03 16:02:24 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -37,8 +37,8 @@ character = (Character*)[CCBReader load:@"Character"];
         
         // Then reset the timer.
         timeSinceObstacle = 0.0f;
-    } 
-//}
+    }
+}
 
 // put new methods here
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
